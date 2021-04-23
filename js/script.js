0257f50a201a16bb9e62686c72117939182dae85
Vladyslav Okopnyy <harmfulcati@gmail.com>
2021-04-23 15:51:20 +0300

    fix

--- js/script.js
+++ js/script.js
@@ -1,81 +1,65 @@
-
-
 function initVue() {
-
   new Vue({
-
-    el: '#app',
+    el: "#app",
     data: {
-
-      'films':[],
-      'serials': [],
-      'search':'',
-      'headline1': 'Films',
-      'headline2': 'Serials',
+      films: [],
+      serials: [],
+      search: "",
+      headline1: "Films",
+      headline2: "Serials",
     },
     methods: {
+      mounted: function () {
+        axios
+          .get("https://api.themoviedb.org/3/search/movie?", {
+            params: {
+              api_key: "905462c907d5c9b0fa16ff95a8dfde69",
+              query: this.search,
+            },
+          })
 
-      mounted: function(){
-
-        axios.get('https://api.themoviedb.org/3/search/movie?', {
-          params: {
-            'api_key': '905462c907d5c9b0fa16ff95a8dfde69',
-            'query': this.search
-          }
-        })
-
-          .then(data => {
-
+          .then((data) => {
             this.films = data.data.results;
             console.log(this.films);
           })
-          .catch(function(e){
-
+          .catch(function (e) {
             this.error = e;
           });
 
-          axios.get('https://api.themoviedb.org/3/search/tv?', {
+        axios
+          .get("https://api.themoviedb.org/3/search/tv?", {
             params: {
-              'api_key': '905462c907d5c9b0fa16ff95a8dfde69',
-              'query': this.search
-            }
+              api_key: "905462c907d5c9b0fa16ff95a8dfde69",
+              query: this.search,
+            },
           })
 
-            .then(data => {
-
-              this.serials = data.data.results;
-              console.log(this.serials);
-
-            })
-            .catch(function(e){
-
-              this.error = e;
-            });
-
+          .then((data) => {
+            this.serials = data.data.results;
+            console.log(this.serials);
+          })
+          .catch(function (e) {
+            this.error = e;
+          });
       },
-      submit: function(){
+      submit: function () {
         this.mounted();
       },
       flag: function (language) {
-        if (language == 'en') {
+        if (language == "en") {
           return 'Language: <img src="img/en.png" alt="Bandiera-Inglese">';
-        }
-        else if (language == 'it'){
+        } else if (language == "it") {
           return 'Language: <img src="img/it.png" alt="Bandiera-Italiana">';
         }
-
       },
       value: function (numberValue) {
-        return parseInt(Math.round(numberValue/2));
-      }
-    }
-
+        return parseInt(Math.round(numberValue / 2));
+      },
+    },
   });
-
 }
 function init() {
-
   initVue();
 }
 
-document.addEventListener('DOMContentLoaded', init);
+document.addEventListener("DOMContentLoaded", init);
